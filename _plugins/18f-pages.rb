cda9f86bc811927c8bc1f6bc9e97bedcaa588b5c
Nick Bristow <Nick.Bristow@gsa.gov>
2017-05-15 11:25:02 -0400

    removed special pre render for federalists

--- _plugins/18f-pages.rb
+++ _plugins/18f-pages.rb
@@ -1,7 +1,7 @@
 PAGES_BRANCHES = ['18f-pages']
 
-if PAGES_BRANCHES.include? ENV['BRANCH']
-    Jekyll::Hooks.register :site, :pre_render do |site|
-        site.config['baseurl'] = '/accessibility'
-    end
-end
+# if PAGES_BRANCHES.include? ENV['BRANCH']
+#     Jekyll::Hooks.register :site, :pre_render do |site|
+#         site.config['baseurl'] = '/accessibility'
+#     end
+# end
