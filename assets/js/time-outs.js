2be909a3d37c7219ff298fb4237831b88e54213b
Matt Henry <matt.henry@gsa.gov>
2019-12-30 13:21:51 -0500

    remove jquery dependency

--- assets/js/time-outs.js
+++ assets/js/time-outs.js
@@ -1,15 +1,22 @@
-$(document).ready(function(){
-  var timer;
-  $('#pForm input').on('input',function(){
-    if (timer)
-      clearTimeout(timer);
-    timer = setTimeout(function(){
-      var c = confirm('Would you like more time to complete the form?'),
-      time;
-      if(c == true)
+window.addEventListener('DOMContentLoaded', function () {
+  var timer,
+    pForm = document.getElementById('pForm');
+
+  Array.from(pForm.getElementsByTagName('input')).map(function (el) {
+    el.addEventListener('input', function () {
+      if (timer) {
         clearTimeout(timer);
-      else
-        $('#pForm').trigger('reset');
-    }, 2000);
+      }
+
+      timer = setTimeout(function () {
+        var c = confirm('Would you like more time to complete the form?'),
+          timer
+        if (c == true) {
+          clearTimeout(timer);
+        } else {
+          pForm.reset();
+        }
+      }, 2000);
+    })
   });
 });
